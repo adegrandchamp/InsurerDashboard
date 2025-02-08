b3d84afbdf68a80f71a1dc25e242b39f28f68299
Alexandra DeGrandchamp <adegrandchamp@gmail.com>
2025-02-08 03:25:40 +0000

    Int_locations including data tests. Added needed lookups to supp_sources .js

--- definitions/sources/capstone_supp_sources.js
+++ definitions/sources/capstone_supp_sources.js
@@ -2,7 +2,9 @@
     "icd_diagnoses",
     "seed_months_of_year",
     "seed_years",
-    "ssa_to_fips_crosswalk"
+    "ssa_to_fips_crosswalk",
+    "state_code_lookup",
+    "zip_to_fips"
 ].forEach((name) =>
     declare({
         databse: "spatial-earth-449020-m3",
